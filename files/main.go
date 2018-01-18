35c77a1c6c32b6339165955882dc851bc00b7d0d
Tomasz Wlodarczyk <twl@phase-ii.com>
2018-01-18 22:00:49 +0000

    Little refactoring

--- files/main.go
+++ files/main.go
@@ -5,7 +5,7 @@ import (
 	config "github.com/tomekwlod/dropboxCleaner/config"
 )
 
-const PERPAGE = 1000
+const perpage = 1000
 
 func Search(term string) []*dropbox.SearchMatch {
 	c := config.DropboxClient()
@@ -14,20 +14,18 @@ func Search(term string) []*dropbox.SearchMatch {
 	var i, start uint64 = 0, 0
 
 	for {
-		start = i * PERPAGE
+		start = i * perpage
 
 		out, err := c.Files.Search(&dropbox.SearchInput{
 			Path:       "/",
 			Query:      term,
-			MaxResults: PERPAGE,
+			MaxResults: perpage,
 			Start:      start,
 		})
 
 		if err != nil {
 			panic(err)
 		}
-
-		// log.Printf("Page: %d / Results: %d", i+1, len(out.Matches))
 
 		for _, file := range out.Matches {
 			files = append(files, file)
